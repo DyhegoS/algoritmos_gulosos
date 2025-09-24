d53c11a07d3bc2c5e5c3abb1cc0aed131ee4aab0
DyhegoS <dyhegodasilva@gmail.com>
2025-09-24 18:01:03 -0300

    iniciado exercicio 4

---
+++ largest_number_possible.js
@@ -0,0 +1,22 @@
+var findLargest = function(n, s){
+    
+}
+
+const entrada1 = {
+    "n": 2,
+    "s": 9
+}
+
+const entrada2 = {
+    "n": 3,
+    "s": 20
+}
+
+const entrada3 = {
+    "n": 4,
+    "s": 0
+}
+
+console.log(findLargest(entrada1.n, entrada1.s))
+console.log(findLargest(entrada2.n, entrada2.s))
+console.log(findLargest(entrada3.n, entrada3.s))
