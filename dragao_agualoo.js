59ea23a623744a72f128735692e8e2b4750d27b6
DyhegoS <dyhegodasilva@gmail.com>
2025-10-10 12:50:28 -0300

    feito exercico policia e ladrão e sua correção

---
+++ dragao_agualoo.js
@@ -0,0 +1,42 @@
+function agualoo(n, m, diameters, heights) {
+
+}
+
+const entrada1 = {
+    "n": 2,
+    "m": 3,
+    "diameters": [5, 4],
+    "heights": [7, 8, 4]
+};
+
+const entrada2 = {
+    "n": 2,
+    "m": 1,
+    "diameters": [5, 5],
+    "heights": [10]
+};
+const entrada3 = {
+    "n": 2,
+    "m": 4,
+    "diameters": [7, 2],
+    "heights": [4, 3, 1, 2]
+};
+const entrada4 = {
+    "n": 2,
+    "m": 4,
+    "diameters": [7, 2],
+    "heights": [2, 1, 8, 5]
+};
+const entrada5 = {
+    "n": 2,
+    "m": 10,
+    "diameters": [1234567, 2345],
+    "heights": [12345610, 1, 123, 23564,
+        123456, 123, 2, 3, 2, 1]
+};
+
+console.log(agualoo(entrada1.n, entrada1.m, entrada1.diameters, entrada1.heights));
+console.log(agualoo(entrada2.n, entrada2.m, entrada2.diameters, entrada2.heights));
+console.log(agualoo(entrada3.n, entrada3.m, entrada3.diameters, entrada3.heights));
+console.log(agualoo(entrada4.n, entrada4.m, entrada4.diameters, entrada4.heights));
+console.log(agualoo(entrada5.n, entrada5.m, entrada5.diameters, entrada5.heights));
--- police_and_thieves.js
+++ police_and_thieves.js
@@ -1,26 +1,42 @@
 var catchThieves = function (arr, n, k) {
     let count = 0;
     let thieves = []
-    
-    for(let i = 0; i < n; i++){
-        if(arr[i] === "T"){
-            thieves.push("T");
-        }
-    }
 
-    if(thieves.length > 0){
-        for(let i = 0; i < n; i++){
-            if(arr[i] === "P"){
-                
+    for (let i = 0; i < n; i++) {
+        if (arr[i] == "T") {
+            thieves.push(arr[i]);
+        }
+    }
+   
+    for (let i = 0; i < n; i++) {
+        if (arr[i] == "P" && thieves.length > 0) {
+            let range = k;
+            while (range > 0) {
+                if (arr[i + range] == "T") {
+                    count++;
+                    thieves.pop();
+                    break;
+                }
+                else if (arr[i - range] == "T") {
+                    count++;
+                    thieves.pop();
+                    break;
+                }
+                else{
+                    range--;
+                }
             }
+            
         }
     }
     
+    return count;
+
 }
 
 const entrada1 = {
     "n": 5,
-    "k": 1,
+    "k": 2,
     "arr": ["P", "T", "T", "P", "T"]
 }
 const entrada2 = {
@@ -29,5 +45,12 @@ const entrada2 = {
     "arr": ["T", "T", "P", "P", "T", "P"]
 }
 
+const entrada3 = {
+    "n": 6,
+    "k": 3,
+    "arr": ["P", "T", "P", "T", "T", "P"]
+}
+
 console.log(catchThieves(entrada1.arr, entrada1.n, entrada1.k))
 console.log(catchThieves(entrada2.arr, entrada2.n, entrada2.k))
+console.log(catchThieves(entrada3.arr, entrada3.n, entrada3.k))
